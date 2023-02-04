a77c9fad824703a89fef121fd4269b61cd0e042f
HaruGrimgar <rewcom05@gmail.com>
2023-02-05 01:36:07 +0300

    ДЗ ко 2-ому уроку

---
+++ Task_2/Program.cs
@@ -0,0 +1,32 @@
+﻿/* 
+Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введите число -> ");
+string num_str = Console.ReadLine();
+int num = Convert.ToInt32(num_str);
+// Проверка на наличие третьего числа
+if (num < 100)
+{
+    Console.WriteLine($"третьей цифры нет у числа {num}");
+}
+else
+{
+    int num_copy = num;
+    int counter = 0;
+    // Узнаём сколько цифр всего в числе
+
+    while (num_copy > 0)
+    {
+        num_copy /= 10;
+        counter += 1;
+    }
+// ToInt32(Math...) - для перевода результата метода 
+int result = num / Convert.ToInt32(Math.Pow(10, counter - 3)) % 10;
+Console.WriteLine(counter);
+Console.Write($"Третья цифра числа {num} -> {result}");
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Введите цифру дня недели -> ");
+string num_str = Console.ReadLine();
+int num = Convert.ToInt32(num_str);
+
+if (num <= 5)
+{
+    Console.WriteLine($"{num} -> не выходной");
+}
+else
+{
+    Console.WriteLine($"{num} -> выходной");
+}
